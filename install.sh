3ddf43d1f52d8ad6a7f446fa7e29b4e447df7a00
PinkP4nther <0x0090@protonmail.com>
2024-09-16 21:28:45 -0700

    fix

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ sudo apt -y install python3 gdb strace ltrace binutils python3-pwntools vim ncat
 # Install pwndbg
 git clone https://github.com/pwndbg/pwndbg ~/pwndbg
 chmod 700 ~/pwndbg/setup.sh
-~/pwndbg/setup.sh
+cd ~/pwndbg
+./setup.sh
